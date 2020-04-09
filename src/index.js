d3f73789143e2af87c0e6f0e1b4170730db858d3
yingliyu <ylyu@sstir.cn>
2020-04-09 15:01:12 +0800

    :bug: del

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
+import Home from '@/views/home'
+
+function About() {
+  return <h2>About</h2>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <p>hello world</p>
-  </React.StrictMode>,
+  <div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  </div>,
   document.getElementById('root')
 )
 
