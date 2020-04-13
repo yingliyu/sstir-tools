1d3817d3ce2ff5207b0ce554b5e867846abce438
yingliyu <ylyu@sstir.cn>
2020-04-13 14:32:10 +0800

    add search result file

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import css from './index.module.less'
 import SearchInput from '@/components/search-input'
+import { Divider } from 'antd'
 export default function Home() {
   return (
     <div className={css['home-wrapper']}>
@@ -10,6 +11,20 @@ export default function Home() {
       <div className={css['home-main']}>
         <h2>开始测评</h2>
         <SearchInput />
+        <div className={css['tips-wrapper']}>
+          <Divider
+            type="vertical"
+            style={{ width: '5px', backgroundColor: '#90c0f4', marginLeft: 0 }}
+          />
+          <span>温馨提示：</span>
+          <div className={css['tips-desc']}>
+            <h4>科研选题分析助手：</h4>
+            <ul>
+              <li>从最新研究文献趋势看选题</li>
+              <li>从基金项目趋势看选题</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/views/search-result/index.jsx
+++ src/views/search-result/index.jsx
@@ -1,4 +1,82 @@
 import React from 'react'
+// import { Space, Button, Tabs, Row, Col } from 'antd'
+import { Space, Button, Tabs } from 'antd'
+const { TabPane } = Tabs
+import css from './index.module.less'
+import SearchInput from '@/components/search-input'
+function callback(key) {
+  console.log(key)
+}
 export default function SearchReasult() {
-  return <div>this is search result !</div>
+  return (
+    <div className={css['search-list-wrapper']}>
+      <div className={css['banner-wrapper']}>
+        <h2>科研助手</h2>
+      </div>
+
+      <div className={css['search-main-wrapper']}>
+        <h2>开始测评</h2>
+        <SearchInput />
+        <div className={css['field-wrapper']}>
+          <span className={css['field-label']}>研究领域：</span>
+          <Space>
+            <Button type="primary" shape="round">
+              人工智能
+            </Button>
+            <Button shape="round">数据挖掘</Button>
+            <Button shape="round">聚类</Button>
+          </Space>
+        </div>
+        <div className={css['tab-wrapper']}>
+          <Tabs defaultActiveKey="1" onChange={callback}>
+            <TabPane tab="Tab 1" key="1">
+              Content of Tab Pane 1
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs>
+          {/* <Tabs defaultActiveKey="1" onChange={callback}>
+            <TabPane tab="可视化分析" key="1">
+              <Row gutter={16}>
+                <Col className={css['gutter-row']} span={8}>
+                  <div className={css['report-item']}>论文-研究走势</div>
+                </Col>
+                <Col span={8} className={css['gutter-row']}>
+                  <div className={css['report-item']}>论文-关联研究</div>
+                </Col>
+                <Col span={8} className={css['gutter-row']}>
+                  <div className={css['report-item']}>论文-高发作者</div>
+                </Col>
+              </Row>
+              <br />
+              <Row gutter={16}>
+                <Col className={css['gutter-row']} span={8}>
+                  <div className={css['report-item']}>论文-高发文机构</div>
+                </Col>
+                <Col span={8} className={css['gutter-row']}>
+                  <div className={css['report-item']}>科研获批趋势</div>
+                </Col>
+                <Col span={8} />
+              </Row>
+            </TabPane>
+            <TabPane tab="论文" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="数据挖掘" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs> */}
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  )
 }
