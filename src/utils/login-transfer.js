8140673bd725845658ab014b4fe9d3cbb333f261
yingliyu <ylyu@sstir.cn>
2020-05-11 18:36:20 +0800

    ie兼容

--- src/utils/login-transfer.js
+++ src/utils/login-transfer.js
@@ -4,6 +4,7 @@ import urls from '@/utils/url-creator'
 function loginMethod() {
   Cookies.set('CURRENTURL', window.location.href)
   window.location.href = urls.casUrl + encodeURIComponent(urls.loginUrl)
+  // window.open(urls.casUrl + encodeURIComponent(urls.loginUrl), '_blank')
 }
 function loginUrl() {
   Cookies.set('CURRENTURL', window.location.href)
